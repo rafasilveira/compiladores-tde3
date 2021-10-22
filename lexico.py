03e4bb18f21f498ed3c2710fccd96744388704fe
rafasilveira <oi@rafasilveira.dev>
2021-10-22 17:39:52 -0300

    lexico

--- lexico.py
+++ lexico.py
@@ -1,6 +1,6 @@
 from io import TextIOWrapper
 from tokens import Token
-from palavras_reservadas import reservadas, palavra_reservada
+from palavras_reservadas import palavra_reservada
 
 
 class Lexico:
@@ -9,9 +9,8 @@ class Lexico:
         self.token = ''
         self.lex = ''
         self.flag_igual = False
-
         self.estado = 0
-        self.c = '\0'
+        self.char_lido = '\0'
 
     def le_token(self):
         self.token = self.__le_token()
@@ -29,134 +28,134 @@ class Lexico:
         while (True):
 
             if (self.estado == 0):
-                if (self.c == ','):
-                    self.c = self.__le_char()
+                if (self.char_lido == ','):
+                    self.char_lido = self.__le_char()
                     return Token.TK_virgula
 
-                if (self.c == '+'):
-                    self.c = self.__le_char()
-                    if (self.c == '='):
-                        self.c = self.__le_char()
+                if (self.char_lido == '+'):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '='):
+                        self.char_lido = self.__le_char()
                         return Token.TK_Mais_Ig
                     return Token.TK_Mais
 
-                if (self.c == '-'):
-                    self.c = self.__le_char()
-                    if (self.c == '='):
-                        self.c = self.__le_char()
+                if (self.char_lido == '-'):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '='):
+                        self.char_lido = self.__le_char()
                         return Token.TK_Menos_Ig
                     return Token.TK_Menos
 
-                if (self.c == '/'):
-                    self.c = self.__le_char()
+                if (self.char_lido == '/'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Div
 
-                if (self.c == '%'):
-                    self.c = self.__le_char()
+                if (self.char_lido == '%'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Rest_Div
 
-                if (self.c == '*'):
-                    self.c = self.__le_char()
+                if (self.char_lido == '*'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Mult
 
-                if (self.c == '{'):
-                    self.c = self.__le_char()
+                if (self.char_lido == '{'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Abre_Chaves
 
-                if (self.c == '}'):
-                    self.c = self.__le_char()
+                if (self.char_lido == '}'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Fecha_Chaves
 
-                if (self.c == ';'):
-                    self.c = self.__le_char()
+                if (self.char_lido == ';'):
+                    self.char_lido = self.__le_char()
                     self.lex = ';'
                     return Token.TK_pv
 
-                if (self.c == '['):
-                    self.c = self.__le_char()
+                if (self.char_lido == '['):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Abre_Colch
 
-                if (self.c == ']'):
-                    self.c = self.__le_char()
+                if (self.char_lido == ']'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Fecha_Colch
 
-                if (self.c == '('):
-                    self.c = self.__le_char()
+                if (self.char_lido == '('):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Abre_Par
 
-                if (self.c == ')'):
-                    self.c = self.__le_char()
+                if (self.char_lido == ')'):
+                    self.char_lido = self.__le_char()
                     return Token.TK_Fecha_Par
 
-                if (self.c == '='):
-                    self.c = self.__le_char()
-                    if (self.c == '='):
-                        self.c = self.__le_char()
+                if (self.char_lido == '='):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '='):
+                        self.char_lido = self.__le_char()
                         self.flag_igual = True
                         return Token.TK_Igual
                     self.lex = '='
                     return Token.TK_Atrib
 
-                if (self.c == '<'):
-                    self.c = self.__le_char()
-                    if (self.c == '='):
-                        self.c = self.__le_char()
+                if (self.char_lido == '<'):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '='):
+                        self.char_lido = self.__le_char()
                         return Token.TK_Menor_Igual
                     return Token.TK_Menor
 
-                if (self.c == '>'):
-                    self.c = self.__le_char()
-                    if (self.c == '='):
-                        self.c = self.__le_char()
+                if (self.char_lido == '>'):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '='):
+                        self.char_lido = self.__le_char()
                         return Token.TK_Maior_Igual
                     return Token.TK_Maior
 
-                if (self.c == '&'):
-                    self.c = self.__le_char()
-                    if (self.c == '&'):
-                        self.c = self.__le_char()
+                if (self.char_lido == '&'):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '&'):
+                        self.char_lido = self.__le_char()
                         return Token.TK_and_logico
                     return Token.TK_and_bitwise
 
-                if (self.c == '!'):
-                    self.c = self.__le_char()
-                    if (self.c == '='):
-                        self.c = self.__le_char()
+                if (self.char_lido == '!'):
+                    self.char_lido = self.__le_char()
+                    if (self.char_lido == '='):
+                        self.char_lido = self.__le_char()
                         return Token.TK_Diferente
 
-                if (self.c >= 'a' and self.c <= 'z' or self.c >= 'A' and self.c <= 'Z' or self.c == '_'):
-                    self.lex = self.c
-                    self.c = self.__le_char()
+                if (self.char_lido >= 'a' and self.char_lido <= 'z' or self.char_lido >= 'A' and self.char_lido <= 'Z' or self.char_lido == '_'):
+                    self.lex = self.char_lido
+                    self.char_lido = self.__le_char()
                     self.estado = 1
                     print(self.estado)
                     continue
 
-                if (self.c >= '0' and self.c <= '9'):
-                    self.lex = self.c
-                    self.c = self.__le_char()
+                if (self.char_lido >= '0' and self.char_lido <= '9'):
+                    self.lex = self.char_lido
+                    self.char_lido = self.__le_char()
                     self.estado = 2
                     continue
 
-                if (self.c == '~'):
+                if (self.char_lido == '~'):
                     return Token.TK_Fim_Arquivo
 
-                if (self.c == '\n' or self.c == '\r' or self.c == '\t' or self.c == '\0' or self.c == ' '):
-                    self.c = self.__le_char()
+                if (self.char_lido == '\n' or self.char_lido == '\r' or self.char_lido == '\t' or self.char_lido == '\0' or self.char_lido == ' '):
+                    self.char_lido = self.__le_char()
                     continue
 
             elif (self.estado == 1):
-                if (self.c >= 'a' and self.c <= 'z' or self.c >= 'A' and self.c <= 'Z' or self.c == '_' or self.c >= '0' and self.c <= '9'):
-                    self.lex += self.c
-                    self.c = self.__le_char()
+                if (self.char_lido >= 'a' and self.char_lido <= 'z' or self.char_lido >= 'A' and self.char_lido <= 'Z' or self.char_lido == '_' or self.char_lido >= '0' and self.char_lido <= '9'):
+                    self.lex += self.char_lido
+                    self.char_lido = self.__le_char()
                     continue
                 else:
                     self.estado = 0
                     return palavra_reservada(self.lex)
 
             elif (self.estado == 2):
-                if (self.c >= '0' and self.c <= '9'):
-                    self.lex += self.c
-                    self.c = self.__le_char()
+                if (self.char_lido >= '0' and self.char_lido <= '9'):
+                    self.lex += self.char_lido
+                    self.char_lido = self.__le_char()
                     continue
 
                 else:
