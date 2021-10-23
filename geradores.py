b22b57b3981e732cc44be29a4a295980daf95c08
rafasilveira <oi@rafasilveira.dev>
2021-10-22 21:50:46 -0300

    fix label output

--- geradores.py
+++ geradores.py
@@ -4,11 +4,13 @@ temp_num = 0
 
 def gera_label():
     global label_num
+    out = f"LB00{label_num}"
     label_num += 1
-    return f"LB00{label_num}"
+    return out
 
 
 def gera_temp():
     global temp_num
+    out = f"T00{temp_num}"
     temp_num += 1
-    return f"T00{temp_num}"
+    return out
