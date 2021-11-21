470b9586213d2c30d592c6fc5dfaac630588f66b
Lucas W. Molin <lwmolin@gmail.com>
2021-11-21 18:41:07 -0300

    Update arquivo.c

--- arquivo.c
+++ arquivo.c
@@ -1,11 +1,17 @@
-for (i = 0, j = 0; i < 10, j < 10; i = i + 1, j = j - 1) {
-  a = 1;
-  if (a > 0) continue;
-  b = 1;
+int calcular(int c){
+    d = c;
+}
 
-  while (a < 10) {
-    a = a + 1;
-  }
-  if (b > 1) break;
-  c = 1;
+int main(int a, int b){
+    for (i = 0, j = 0; i < 10, j < 10; i = i + 1, j = j - 1) {
+      a = 1;
+      if (a > 0) continue;
+      b = 1;
+
+      while (a < 10) {
+        a = a + 1;
+      }
+      if (b > 1) break;
+      c = 1;
+    }
 }
