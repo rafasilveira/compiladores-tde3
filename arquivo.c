c995d2c25dd50a0ee981099f963038ab0f198ab0
Lucas W. Molin <lwmolin@gmail.com>
2021-12-04 18:33:45 -0300

    Update arquivo.c

--- arquivo.c
+++ arquivo.c
@@ -1,14 +1,15 @@
 int calcular(int a, int b){
-    e = (a * 3) + b;
+    int e = (a * 3) + b;
+    return e;
 }
 
 void print(int a){
-    s = a + b;
+    int s = a + b;
 }
 
 int main(){
     for (i = 0; i < 10; i = i + 1) {
-        z = calcular(i, 3);
+        int z = calcular(i, 3);
     }
 
     print(z);
